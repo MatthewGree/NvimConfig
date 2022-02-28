7721c585b966d5dae7f823a637e9eec6e95e880d
Mateusz Kmak <hamsterkmak@gmail.com>
2022-02-28 14:37:39 +0100

    plugins and options

---
+++ lua/matt/options.lua
@@ -0,0 +1,17 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 2
+vim.opt.completeopt = {"menuone", "noselect"}
+vim.opt.fileencoding = "utf-8"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.updatetime = 300
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
